2c60e5865bad271802a8991aebddaa6fc9ccde52
Jan Kuri <jkuri88@gmail.com>
2024-05-02 22:12:06 +0200

    Initial nextjs version

--- api/index.js
+++
@@ -1,3 +0,0 @@
-import { app } from '../dist/app/server/server.mjs';
-
-export default app();
---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { GoogleAnalytics } from '@next/third-parties/google';
+import { Head, Html, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+      <GoogleAnalytics gaId="UA-60251558-1" />
+    </Html>
+  );
+}
